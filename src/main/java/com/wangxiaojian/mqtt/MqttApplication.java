477439a403a7b2429718736be31c0596d12783af
wang-xiaojian <sinian900902@126.com>
2020-01-06 17:54:03 +0800

    测试一下提交

--- src/main/java/com/wangxiaojian/mqtt/MqttApplication.java
+++ src/main/java/com/wangxiaojian/mqtt/MqttApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MqttApplication {
 
     public static void main(String[] args) {
+        System.out.println("测试一下启动");
         SpringApplication.run(MqttApplication.class, args);
     }
 
